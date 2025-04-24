3300ff2bb5fee0f8726ef7a75e3f028232a23996
Andrew B. Cooper <andrewbcooper@users.noreply.github.com>
2025-04-23 21:13:59 -0700

    reading RDS

--- app.R
+++ app.R
@@ -1,11 +1,11 @@
 library(shiny)
 ui <- fluidPage(
-  "Hello, world!"#,
-  #verbatimTextOutput("silly")
+  "Hello, world!",
+  verbatimTextOutput("silly")
 )
 server <- function(input, output, session) {
-  #output$silly <- renderPrint({
-  #  readRDS('mysillyfile')
-  #})
+  output$silly <- renderPrint({
+    readRDS('mysillyfile')
+  })
 }
 shinyApp(ui, server)
