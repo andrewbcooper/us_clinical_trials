9022b31599889ae8eafc643d1497b2a0031fcf4d
Andrew B. Cooper <andrewbcooper@users.noreply.github.com>
2025-04-23 21:10:21 -0700

    adding the app

---
+++ app.R
@@ -0,0 +1,11 @@
+library(shiny)
+ui <- fluidPage(
+  "Hello, world!"#,
+  #verbatimTextOutput("silly")
+)
+server <- function(input, output, session) {
+  #output$silly <- renderPrint({
+  #  readRDS('mysillyfile')
+  #})
+}
+shinyApp(ui, server)
